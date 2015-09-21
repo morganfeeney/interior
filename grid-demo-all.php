7f9eaa6e6c814d3f18a355f6eb9682782b50cde2
Morgan Feeney <me@morganfeeney.com>
2015-09-21 23:42:18 +0100

    Added live reload script

--- grid-demo-all.php
+++ grid-demo-all.php
@@ -335,6 +335,7 @@
 		<footer class="container-outer">
 			<div class="container-set-width">
 			</div>
+			<script src="http://localhost:35729/livereload.js"></script>
 			<script type="text/javascript" src="js/functions.js"></script>
 		</footer>
 	</body>
