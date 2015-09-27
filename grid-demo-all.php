9121574018975d2a00a7c344f71150328e570c36
Morgan Feeney <me@morganfeeney.com>
2015-09-27 15:08:35 +0100

    Added vertical spacing, and added some nice demo counters.

--- grid-demo-all.php
+++ grid-demo-all.php
@@ -33,7 +33,7 @@
 					<p>
 						Leaving the default values in place, e.g. <code class="language-scss">$grid-cols: 12</code> will produce a 12 column grid, with a <strong>total</strong> gutter width of .8rem, with grid columns aligned from the left:
 					</p>
-					<section class="grid grid-nums demo demo-bg-1">
+					<div class="grid grid-nums demo demo-bg-1">
 						<div class="grid-outer">
 							<div class="grid-row clearfix">
 								<div class="grid-col-12">
@@ -132,39 +132,44 @@
 								</div>
 							</div>
 						</div>
-					</section>
+					</div>
 					<h2>Modular mixins</h2>
 					<p>Interior can be re-used over and over again, by declaring your desired units within a mixin you can leave the defaults in place, knowing that they will be overridden.</p>
 					<?php include('includes/code-samples/interior-config-1.php') ?>
-					<h2>As simple as you make it</h2>
-					<p>
-						Create simple layouts, and output as many columns as required, nested within in as many different classes as needed.
-					</p>
-					<section class="grid grid-nums demo demo-bg-1 simple-demo-1">
-						<div class="grid-outer">
-							<div class="grid-row clearfix">
-								<div class="grid-col-1">
-									<div class="inner"></div>
-								</div>
-								<div class="grid-col-2">
-									<div class="inner"></div>
-								</div>
-								<div class="grid-col-3">
-									<div class="inner"></div>
+					<div class="accompanying-text">
+						<h2>As simple as you make it</h2>
+						<p>
+							Create simple layouts, and output as many columns as required, nested within in as many different classes as needed.
+						</p>
+					</div>
+					<div class="visual-example">
+						<div class="grid grid-nums demo demo-bg-1 simple-demo-1">
+							<div class="grid-outer">
+								<div class="grid-row clearfix">
+									<div class="grid-col-1">
+										<div class="inner"></div>
+									</div>
+									<div class="grid-col-2">
+										<div class="inner"></div>
+									</div>
+									<div class="grid-col-3">
+										<div class="inner"></div>
+									</div>
 								</div>
 							</div>
 						</div>
-					</section>	
+					</div>
 					<?php include('includes/code-samples/interior-config-2.php') ?>
-			
 				</section>
 
 				<section class="grid demo demo-bg-2">
-					<h2>Group nested columns</h2>
-					<p>
-						We have to use different classes <code class="language-scss">.col-single</code> &amp; <code class="language-scss">.col-group</code> for parent containers due to the amount of padding created when nesting. This enables us to group different numbers of columns, without affecting the gutter widths:
-					</p>
-					<section>
+					<div class="accompanying-text">
+						<h2>Group nested columns</h2>
+						<p>
+							We have to use different classes <code class="language-scss">.col-single</code> &amp; <code class="language-scss">.col-group</code> for parent containers due to the amount of padding created when nesting. This enables us to group different numbers of columns, without affecting the gutter widths:
+						</p>
+					</div>
+					<div class="visual-example">
 						<div class="grid-outer">
 							<div class="grid-row clearfix">
 								<div class="grid-col-4 col-single with-names">
@@ -183,97 +188,101 @@
 								</div>
 							</div>
 						</div>
-					</section>
+					</div>
 					<?php include('includes/code-samples/nested-demo-1.php') ?>
 				</section>
 
 				<!-- Demo 2 -->
 
 				<section class="grid demo demo-bg-1">
-					<h2>Nest columns within columns</h2>
-					<p>
-						In this example I have re-used the grouping classes from the previous example <code class="language-scss">.col-single</code> &amp; <code class="language-scss">.col-group</code>:
-					</p>
-					<div class="grid-outer demo-bg-1">
-						<div class="grid-row">
-							<div class="grid-col-12 col-single with-names">
-								<div class="inner"></div>
-							</div>
-						</div>
-						<div class="grid-row">
-							<div class="grid-col-12 col-group">
-								<div class="grid-row">
-									<div class="grid-col-1">
-										<div class="inner"></div>
-									</div>
-									<div class="grid-col-1">
-										<div class="inner"></div>
-									</div>
-									<div class="grid-col-1">
-										<div class="inner"></div>
-									</div>
-									<div class="grid-col-1">
-										<div class="inner"></div>
-									</div>
-									<div class="grid-col-1">
-										<div class="inner"></div>
-									</div>
-									<div class="grid-col-1">
-										<div class="inner"></div>
-									</div>
-									<div class="grid-col-1">
-										<div class="inner"></div>
-									</div>
-									<div class="grid-col-1">
-										<div class="inner"></div>
-									</div>
-									<div class="grid-col-1">
-										<div class="inner"></div>
-									</div>
-									<div class="grid-col-1">
-										<div class="inner"></div>
-									</div>
-									<div class="grid-col-1">
-										<div class="inner"></div>
-									</div>
-									<div class="grid-col-1">
-										<div class="inner"></div>
-									</div>
+					<div class="accompanying-text">
+						<h2>Nest columns within columns</h2>
+						<p>
+							In this example I have re-used the grouping classes from the previous example <code class="language-scss">.col-single</code> &amp; <code class="language-scss">.col-group</code>:
+						</p>
+					</div>
+					<div class="visual-example">
+						<div class="grid-outer demo-bg-1">
+							<div class="grid-row">
+								<div class="grid-col-12 col-single with-names">
+									<div class="inner"></div>
 								</div>
 							</div>
-						</div>
-						<div class="grid-row">
-							<div class="grid-col-6 col-group">
-								<div class="col-group with-names clearfix">
+							<div class="grid-row">
+								<div class="grid-col-12 col-group">
 									<div class="grid-row">
-										<div class="grid-col-4">
+										<div class="grid-col-1">
 											<div class="inner"></div>
 										</div>
-										<div class="grid-col-4">
+										<div class="grid-col-1">
 											<div class="inner"></div>
 										</div>
-										<div class="grid-col-4">
+										<div class="grid-col-1">
+											<div class="inner"></div>
+										</div>
+										<div class="grid-col-1">
+											<div class="inner"></div>
+										</div>
+										<div class="grid-col-1">
+											<div class="inner"></div>
+										</div>
+										<div class="grid-col-1">
+											<div class="inner"></div>
+										</div>
+										<div class="grid-col-1">
+											<div class="inner"></div>
+										</div>
+										<div class="grid-col-1">
+											<div class="inner"></div>
+										</div>
+										<div class="grid-col-1">
+											<div class="inner"></div>
+										</div>
+										<div class="grid-col-1">
+											<div class="inner"></div>
+										</div>
+										<div class="grid-col-1">
+											<div class="inner"></div>
+										</div>
+										<div class="grid-col-1">
 											<div class="inner"></div>
 										</div>
 									</div>
 								</div>
 							</div>
-							<div class="grid-col-6 col-group">
-								<div class="col-group with-names clearfix">
-									<div class="grid-row">
-										<div class="grid-col-6">
-											<div class="inner"></div>
+							<div class="grid-row">
+								<div class="grid-col-6 col-group">
+									<div class="col-group with-names clearfix">
+										<div class="grid-row">
+											<div class="grid-col-4">
+												<div class="inner"></div>
+											</div>
+											<div class="grid-col-4">
+												<div class="inner"></div>
+											</div>
+											<div class="grid-col-4">
+												<div class="inner"></div>
+											</div>
 										</div>
-										<div class="grid-col-6">
-											<div class="inner"></div>
+									</div>
+								</div>
+								<div class="grid-col-6 col-group">
+									<div class="col-group with-names clearfix">
+										<div class="grid-row">
+											<div class="grid-col-6">
+												<div class="inner"></div>
+											</div>
+											<div class="grid-col-6">
+												<div class="inner"></div>
+											</div>
 										</div>
 									</div>
 								</div>
 							</div>
-						</div>
-						<div class="grid-row">
-							<div class="grid-col-12 col-single with-names">
-								<div class="inner"></div>
+							<div class="grid-row">
+								<div class="grid-col-12 col-single with-names">
+									<div class="inner"></div>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -282,12 +291,14 @@
 				<!-- Demo 3 -->
 
 				<section class="grid demo demo-bg-3 clearfix">
-					<h2>Offset columns</h2>
-					<p>
-						If you would like to position columns wihout changing <code class="language-scss">float: left</code> to <code class="language-scss">float: right</code>, or using <code class="language-scss">margin: auto</code> with <code class="language-scss">display: block</code> you can use offsets.
-						Offsets use math percentage values to offset columns, combined with a left margin. Offset CSS is standard, however you can turn it off if you don't want the extra bytes.
-					</p>
-					<section>
+					<div class="accompanying-text">
+						<h2>Offset columns</h2>
+						<p>
+							If you would like to position columns wihout changing <code class="language-scss">float: left</code> to <code class="language-scss">float: right</code>, or using <code class="language-scss">margin: auto</code> with <code class="language-scss">display: block</code> you can use offsets.
+							Offsets use math percentage values to offset columns, combined with a left margin. Offset CSS is standard, however you can turn it off if you don't want the extra bytes.
+						</p>
+					</div>
+					<div class="visual-example">
 						<div class="grid-outer">
 							<div class="grid-row">
 								<div class="grid-col-6 grid-col-push-6 with-names">
@@ -305,35 +316,39 @@
 								</div>
 							</div>
 						</div>
-					</section>
+					</div>
 					<?php include('includes/code-samples/offset-demo-1.php') ?>
 				</section>
 
 				<!-- Demo 4 -->
 
 				<section class="grid demo demo-bg-3">
-					<h2>Flip columns</h2>
-					<p>
-						 The usual use case scenario for flipping visual appearance of columns is for use with Responsive Web Design. For example you want the left column to be the second column going down the page once your columns eventually span the full width, at a set break-point. Unless you change the source order you will not be able to achieve this using CSS.
-					</p>
-					<div class="grid-outer">
-						<div class="grid-row">
-							<div class="demo-borders clearfix">
-								<div class="grid-col-8 grid-col-flip-left-4 with-names">
-									<div class="inner"></div>
-								</div>
-								<div class="grid-col-4 grid-col-flip-right-8 with-names">
-									<div class="inner"></div>
+					<div class="accompanying-text">
+						<h2>Flip columns</h2>
+						<p>
+							 The usual use case scenario for flipping visual appearance of columns is for use with Responsive Web Design. For example you want the left column to be the second column going down the page once your columns eventually span the full width, at a set break-point. Unless you change the source order you will not be able to achieve this using CSS.
+						</p>
+					</div>
+					<div class="visual-example">
+						<div class="grid-outer">
+							<div class="grid-row">
+								<div class="demo-borders clearfix">
+									<div class="grid-col-8 grid-col-flip-left-4 with-names">
+										<div class="inner"></div>
+									</div>
+									<div class="grid-col-4 grid-col-flip-right-8 with-names">
+										<div class="inner"></div>
+									</div>
 								</div>
 							</div>
-						</div>
-						<div class="grid-row">
-							<div class="demo-borders clearfix">
-								<div class="grid-col-4 grid-col-flip-left-8 with-names">
-									<div class="inner"></div>
-								</div>
-								<div class="grid-col-8 grid-col-flip-right-4 with-names">
-									<div class="inner"></div>
+							<div class="grid-row">
+								<div class="demo-borders clearfix">
+									<div class="grid-col-4 grid-col-flip-left-8 with-names">
+										<div class="inner"></div>
+									</div>
+									<div class="grid-col-8 grid-col-flip-right-4 with-names">
+										<div class="inner"></div>
+									</div>
 								</div>
 							</div>
 						</div>
