4ce7f500137251172816b958d595ba11e6af343b
Morgan Feeney <me@morganfeeney.com>
2015-10-13 00:25:27 +0100

    Created new example.

--- examples/index.php
+++ examples/index.php
@@ -22,13 +22,13 @@
 							<div class="i-inner">
 								<dl>
 									<dt>
-										<a href="eg1.php">Simple 2 column layout</a>
+										<a href="simple-2-column-layout.php">Simple 2 column layout</a>
 									</dt>
 									<dd>
 										A main content area with an aside.
 									</dd>
 									<dt>
-										<a href="eg2.php">Simple BLAH column layout</a>
+										<a href="product-lister.php">Product lister page layout</a>
 									</dt>
 									<dd>
 										A BLAH content area with an aside.
@@ -36,14 +36,6 @@
 								</dl>
 							</div>
 						</div>
-						<aside class="i-col-12 i-sm-4 eg-bg-1">
-							<div class="i-outer">
-								<div class="i-inner">
-									<h3>This is an aside</h3>
-									<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-								</div>
-							</div>
-						</aside>
 					</div>
 				</div>
 			</div>
---
+++ examples/product-lister.php
@@ -0,0 +1,93 @@
+<?php
+	$pageTitle = 'Layout example 1';
+	$baseUrl = '../';
+	include('../includes/head.php');
+?>
+<body class="eg eg2">
+	<header class="container-outer">
+		<div class="container-width">
+			<div class="container-inner">
+				<div class="i-col-12 i-sm-12">
+					<h1>EG<small>.2</small> <span>Product lister page layout</span></h1>
+				</div>
+			</div>
+		</div>
+	</header>
+	<main class="container-outer">
+		<div class="container-width">
+			<div class="container-inner">
+				<div class="i-outer">
+					<div class="i-row">
+						<div class="i-col-12 i-sm-8 i-g product-lister">
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+								<div class="i-inner">
+								</div>
+							</div>
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+								<div class="i-inner">
+								</div>
+							</div>
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+								<div class="i-inner">
+								</div>
+							</div>
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+								<div class="i-inner">
+								</div>
+							</div>
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+								<div class="i-inner">
+								</div>
+							</div>
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+								<div class="i-inner">
+								</div>
+							</div>
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+								<div class="i-inner">
+								</div>
+							</div>
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+								<div class="i-inner">
+								</div>
+							</div>
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+								<div class="i-inner">
+								</div>
+							</div>
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+								<div class="i-inner">
+								</div>
+							</div>
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+								<div class="i-inner">
+								</div>
+							</div>
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+								<div class="i-inner">
+								</div>
+							</div>
+						</div>
+						<aside class="i-col-12 i-sm-4 i-md-4 eg-bg-1">
+							<div class="i-outer">
+								<div class="i-inner">
+									<h3>This is an aside</h3>
+									<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+								</div>
+							</div>
+						</aside>
+						<div class="i-row">
+							<div class="i-col-12 i-sm-8">
+								<div class="i-inner">
+									<h2>The standard Lorem Ipsum passage, used since the 1500s</h2>
+									<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+	<?php include('../includes/eg-footer.php') ?>
+</body>
