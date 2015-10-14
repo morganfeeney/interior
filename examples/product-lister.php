985925d3c23905ad5f6247dab8fd61f8b1fc16d6
Morgan Feeney <me@morganfeeney.com>
2015-10-15 00:23:28 +0100

    Removed some unnecessary partials, renamed one, worked on the general e.g. theme, added real-world examples to product lister page demo.

--- examples/product-lister.php
+++ examples/product-lister.php
@@ -7,7 +7,7 @@
 	<header class="container-outer">
 		<div class="container-width">
 			<div class="container-inner">
-				<div class="i-col-12 i-sm-12">
+				<div class="i-col-12 i-xs-12 i-sm-12">
 					<h1>EG<small>.2</small> <span>Product lister page layout</span></h1>
 				</div>
 			</div>
@@ -19,56 +19,104 @@
 				<div class="i-outer">
 					<div class="i-row">
 						<div class="i-col-12 i-sm-8 i-g product-lister">
-							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 product-fragment">
 								<div class="i-inner">
+									<h4 class="product-title">This is a product title</h4>
+									<img class="img-r" src="http://placehold.it/200/FFF1F1/FF7070?text=<IMG>">
+									<p class="price">£100.00</p>
+									<button>Buy now</button>
 								</div>
 							</div>
-							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 product-fragment">
 								<div class="i-inner">
+									<h4 class="product-title">This is a product title</h4>
+									<img class="img-r" src="http://placehold.it/200/FFF1F1/FF7070?text=<IMG>">
+									<p class="price">£100.00</p>
+									<button>Buy now</button>
 								</div>
 							</div>
-							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 product-fragment">
 								<div class="i-inner">
+									<h4 class="product-title">This is a really long product title which goes onto several lines</h4>
+									<img class="img-r" src="http://placehold.it/200/FFF1F1/FF7070?text=<IMG>">
+									<p class="price">£100.00</p>
+									<button>Buy now</button>
 								</div>
 							</div>
-							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 product-fragment">
 								<div class="i-inner">
+									<h4 class="product-title">This is a product title</h4>
+									<img class="img-r" src="http://placehold.it/200/FFF1F1/FF7070?text=<IMG>">
+									<p class="price">£100.00</p>
+									<button>Buy now</button>
 								</div>
 							</div>
-							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 product-fragment">
 								<div class="i-inner">
+									<h4 class="product-title">This is a product title</h4>
+									<img class="img-r" src="http://placehold.it/200/FFF1F1/FF7070?text=<IMG>">
+									<p class="price">£100.00</p>
+									<button>Buy now</button>
 								</div>
 							</div>
-							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 product-fragment">
 								<div class="i-inner">
+									<h4 class="product-title">This is a really long product title which goes onto several lines</h4>
+									<img class="img-r" src="http://placehold.it/200/FFF1F1/FF7070?text=<IMG>">
+									<p class="price">£100.00</p>
+									<button>Buy now</button>
 								</div>
 							</div>
-							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 product-fragment">
 								<div class="i-inner">
+									<h4 class="product-title">This is a product title</h4>
+									<img class="img-r" src="http://placehold.it/200/FFF1F1/FF7070?text=<IMG>">
+									<p class="price">£100.00</p>
+									<button>Buy now</button>
 								</div>
 							</div>
-							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 product-fragment">
 								<div class="i-inner">
+									<h4 class="product-title">This is a product title</h4>
+									<img class="img-r" src="http://placehold.it/200/FFF1F1/FF7070?text=<IMG>">
+									<p class="price">£100.00</p>
+									<button>Buy now</button>
 								</div>
 							</div>
-							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 product-fragment">
 								<div class="i-inner">
+									<h4 class="product-title">This is a product title</h4>
+									<img class="img-r" src="http://placehold.it/200/FFF1F1/FF7070?text=<IMG>">
+									<p class="price">£100.00</p>
+									<button>Buy now</button>
 								</div>
 							</div>
-							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 product-fragment">
 								<div class="i-inner">
+									<h4 class="product-title">This is a product title</h4>
+									<img class="img-r" src="http://placehold.it/200/FFF1F1/FF7070?text=<IMG>">
+									<p class="price">£100.00</p>
+									<button>Buy now</button>
 								</div>
 							</div>
-							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 product-fragment">
 								<div class="i-inner">
+									<h4 class="product-title">This is a product title</h4>
+									<img class="img-r" src="http://placehold.it/200/FFF1F1/FF7070?text=<IMG>">
+									<p class="price">£100.00</p>
+									<button>Buy now</button>
 								</div>
 							</div>
-							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 eg-bg-1">
+							<div class="i-col-6 i-xs-4 i-sm-4 i-md-3 product-fragment">
 								<div class="i-inner">
+									<h4 class="product-title">This is a product title</h4>
+									<img class="img-r" src="http://placehold.it/200/FFF1F1/FF7070?text=<IMG>">
+									<p class="price">£100.00</p>
+									<button>Buy now</button>
 								</div>
 							</div>
 						</div>
-						<aside class="i-col-12 i-sm-4 i-md-4 eg-bg-1">
+						<aside class="i-col-12 i-xs-12 i-sm-4 i-md-4 product-fragment">
 							<div class="i-outer">
 								<div class="i-inner">
 									<h3>This is an aside</h3>
@@ -77,7 +125,7 @@
 							</div>
 						</aside>
 						<div class="i-row">
-							<div class="i-col-12 i-sm-8">
+							<div class="i-col-12 i-xs-12 i-sm-8">
 								<div class="i-inner">
 									<h2>The standard Lorem Ipsum passage, used since the 1500s</h2>
 									<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
