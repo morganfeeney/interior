271a8a485159e73a7c0efdca82d1e31152108cca
Morgan Feeney <morgan.feeney@jdwilliams.co.uk>
2016-03-29 17:02:42 +0100

    Adding Nunjucks into the mix

--- gruntfile.js
+++ gruntfile.js
@@ -1,70 +1,81 @@
 module.exports = function(grunt) {
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        sass: {
-            interior: {
-                options: {
-                    style: 'expanded',
-                    sourcemap: 'auto',
-                    precision: 4
-                },
-                files: {
-                    'dist/css/interior.css': 'src/scss/style.scss'
-                }
-            }
-        },
-        postcss: {
-            options: {
-                map: true,
-                processors: [
-                    require('autoprefixer')({
-                        browsers: ['last 2 versions']
-                    })
-                ]
-            },
-            interior: {
-                src: 'dist/css/*.css'
-            }
-        },
-        watch: {
-            sass: {
-                files: ['src/scss/*.scss', 'src/scss/**/*.scss'],
-                tasks: 'sass',
-                options: {
-                    spawn: false,
-                    livereload: true
-                }
-            },
-            html: {
-                files: ['*.html'],
-                tasks: 'watch',
-                options: {
-                    spawn: false,
-                    livereload: true
-                }
-            },
-            css: {
-                files: ['dist/*.css'],
-                tasks: 'postcss',
-                options: {
-                    spawn: false,
-                    livereload: true
-                }
-            },
-        },
-    });
+ // Project configuration.
+ grunt.initConfig({
+  pkg: grunt.file.readJSON('package.json'),
+  sass: {
+   interior: {
+    options: {
+     style: 'expanded',
+     sourcemap: 'auto',
+     precision: 4
+    },
+    files: {
+     'dist/css/interior.css': 'src/scss/style.scss'
+    }
+   }
+  },
+  postcss: {
+   options: {
+    map: true,
+    processors: [
+     require('autoprefixer')({
+      browsers: ['last 2 versions']
+     })
+    ]
+   },
+   interior: {
+    src: 'dist/css/*.css'
+   }
+  },
+  watch: {
+   sass: {
+    files: ['src/scss/*.scss', 'src/scss/**/*.scss'],
+    tasks: 'sass',
+    options: {
+     spawn: false,
+     livereload: true
+    }
+   },
+   css: {
+    files: ['dist/*.css'],
+    tasks: 'postcss',
+    options: {
+     spawn: false,
+     livereload: true
+    }
+   },
+   nunjucks: {
+    options: {
+     data: grunt.file.readJSON('data.json'),
+     paths: 'src'
+    },
+    dev: {
+     files: [{
+      expand: true,
+      cwd: "src/",
+      src: [
+       'templates/**/*.html',
+       'partials/**/*.html'
+      ],
+      dest: "build/",
+      ext: ".html"
+     }],
+    }
+   },
+  },
+ });
 
-    // Load the plugins to run your tasks
-    require('load-grunt-tasks')(grunt, {
-        scope: 'devDependencies'
-    });
-    require('time-grunt')(grunt);
+ // Load the plugins to run your tasks
+ require('load-grunt-tasks')(grunt, {
+  scope: 'devDependencies'
+ });
+ require('time-grunt')(grunt);
 
-    // Default task(s).
-    grunt.registerTask('default', [
-        'sass',
-        'postcss',
-        'watch'
-    ]);
+ // Default task(s).
+ grunt.registerTask('default', [
+  'sass',
+  'nunjucks',
+  'postcss',
+  'watch'
+ ]);
 };
