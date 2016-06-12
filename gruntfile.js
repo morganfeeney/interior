d16fa0918f19a3dce0e13f59d99fc03e956dbd07
Morgan Feeney <me@morganfeeney.com>
2016-06-13 00:17:45 +0100

    Commented out temporarily.

--- gruntfile.js
+++ gruntfile.js
@@ -12,8 +12,8 @@ module.exports = function(grunt) {
       precision: 4
     },
     files: {
-     "css/style.css": "src/scss/style.scss",
-     "css/demo/style.css": "src/scss/demo/style.scss",
+     //"css/style.css": "src/scss/style.scss",
+     //"css/demo/style.css": "src/scss/demo/style.scss",
      "css/prototypes/style.css": "src/scss/prototypes/style.scss"
     }
    }
