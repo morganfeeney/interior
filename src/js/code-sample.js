25386de6211056ae61d4a7b21b92d4aae6a6a2e8
Morgan Feeney <info@virtualsoup.co.uk>
2019-06-04 01:13:33 +0100

    W.I.P. moved template so it can be regenerated??

--- src/js/code-sample.js
+++ src/js/code-sample.js
@@ -1,25 +1,41 @@
+// const wrapper = template.querySelector('.js-code-sample-wrapper');
 const codeSample = document.querySelector('.js-code-sample');
 const codeSampleTemplate = document.querySelector('.js-code-sample-template');
-const template = document.importNode(codeSampleTemplate.content, true);
-const templateBody = template.querySelector('.js-code-sample-modal-body');
 
-executeCodeSampleModal = () => {
-  codeSample.addEventListener('click', function() {
+const codeSampleString =
+  `<template class="js-code-sample-template">
+    <div class="js-code-sample-wrapper code-sample__modal">
+      <button class="js-code-sample-close-modal">Close</button>
+      <div class="js-code-sample-modal-body code-sample__modal-body">
+      </div>
+    </div>
+  </template>`;
 
-    // Clone the code sample we want to display
-    const clone = this.cloneNode(true);
+executeCodeSampleModal = (c) => {
 
-    // Append the cloned code sample to the template body
-    templateBody.appendChild(clone);
+  const template = document.importNode(codeSampleTemplate.content, true);
+  const templateBody = template.querySelector('.js-code-sample-modal-body');
+  const closeModal = template.querySelector('.js-code-sample-close-modal');
+  // Clone the code sample we want to display
+  const clone = c.cloneNode(true);
 
-    // Append all of it to the body
-    body.appendChild(template);
+  // Append the cloned code sample to the template body
+  templateBody.appendChild(clone);
+
+  // Append all of it to the body
+  body.appendChild(template);
+
+  // Remove the modal using a click event
+  closeModal.addEventListener('click', () => {
+    wrapper.remove();
   })
-}
+};
 
-executeCodeSampleModal();
+  window.onload = document.querySelector('body').insertAdjacentHTML('beforeend', codeSampleString);
 
-
+  codeSample.addEventListener('click', function() {
+    executeCodeSampleModal(this);
+  });
 // Need to write some kind of check, possibly boolean, to see if the code has
 // been appended or not, could posibly use a class, or see if content exists?
 // then maybe destriy the modal and do it each time for every instance.
