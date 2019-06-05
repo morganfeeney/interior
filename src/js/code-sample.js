3ea3b38ec330408f14a3ad78788db1f301b7c6a5
Morgan Feeney <info@virtualsoup.co.uk>
2019-06-05 01:24:23 +0100

    Worked out how to add the string, after updating it’s content and then do some more stuff.

--- src/js/code-sample.js
+++ src/js/code-sample.js
@@ -1,41 +1,51 @@
-// const wrapper = template.querySelector('.js-code-sample-wrapper');
 const codeSample = document.querySelector('.js-code-sample');
-const codeSampleTemplate = document.querySelector('.js-code-sample-template');
-
+let maybeabletouseavariablehere;
 const codeSampleString =
   `<template class="js-code-sample-template">
     <div class="js-code-sample-wrapper code-sample__modal">
-      <button class="js-code-sample-close-modal">Close</button>
+      <button class="js-code-sample-close-modal" onClick="closeModal()">Close</button>
       <div class="js-code-sample-modal-body code-sample__modal-body">
+      ${maybeabletouseavariablehere}
       </div>
     </div>
   </template>`;
 
-executeCodeSampleModal = (c) => {
-
-  const template = document.importNode(codeSampleTemplate.content, true);
-  const templateBody = template.querySelector('.js-code-sample-modal-body');
-  const closeModal = template.querySelector('.js-code-sample-close-modal');
-  // Clone the code sample we want to display
-  const clone = c.cloneNode(true);
-
-  // Append the cloned code sample to the template body
-  templateBody.appendChild(clone);
-
-  // Append all of it to the body
-  body.appendChild(template);
-
-  // Remove the modal using a click event
-  closeModal.addEventListener('click', () => {
-    wrapper.remove();
-  })
-};
-
-  window.onload = document.querySelector('body').insertAdjacentHTML('beforeend', codeSampleString);
+executeCodeSampleModal = () => {
+  const template = document.querySelector('.js-code-sample-template');
+  const templateContent = document.importNode(template.content, true);
+  const templateBody = templateContent.querySelector('.js-code-sample-modal-body');
+  const templateWrapper = templateContent.querySelector('.js-code-sample-wrapper');
 
   codeSample.addEventListener('click', function() {
-    executeCodeSampleModal(this);
+
+    // Clone the code sample we want to display
+    const clone = this.cloneNode(true);
+
+    // Append the cloned code sample to the template body
+    templateBody.appendChild(clone);
+let maybeabletouseavariablehere = clone;
+    body.appendChild(templateWrapper);
   });
+};
+
+// Remove the modal using a click event
+closeModal = () => {
+  document.querySelector('.js-code-sample-wrapper').remove();
+}
+
+insertTemplate = () => {
+  return body.insertAdjacentHTML('beforeend', codeSampleString)
+}
+
+
+window.onload = () => {
+  insertTemplate();
+  if (document.querySelector('.js-code-sample-template')) {
+    return executeCodeSampleModal();
+  } else {
+    return
+  }
+}
 // Need to write some kind of check, possibly boolean, to see if the code has
 // been appended or not, could posibly use a class, or see if content exists?
 // then maybe destriy the modal and do it each time for every instance.
