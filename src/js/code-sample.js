1a237aa5b6a41f89e3bd1f648ddea1ff449be727
Morgan Feeney <morgan.feeney@jdwilliams.co.uk>
2019-06-06 16:13:13 +0100

    Moved the template back to the HTML so that I can do all the things I want to do that the string literal was preventing me from doing.

--- src/js/code-sample.js
+++ src/js/code-sample.js
@@ -1,20 +1,11 @@
 const codeSample = document.querySelector('.js-code-sample');
-let maybeabletouseavariablehere;
-const codeSampleString =
-  `<template class="js-code-sample-template">
-    <div class="js-code-sample-wrapper code-sample__modal">
-      <button class="js-code-sample-close-modal" onClick="closeModal()">Close</button>
-      <div class="js-code-sample-modal-body code-sample__modal-body">
-      ${maybeabletouseavariablehere}
-      </div>
-    </div>
-  </template>`;
 
 executeCodeSampleModal = () => {
   const template = document.querySelector('.js-code-sample-template');
   const templateContent = document.importNode(template.content, true);
   const templateBody = templateContent.querySelector('.js-code-sample-modal-body');
   const templateWrapper = templateContent.querySelector('.js-code-sample-wrapper');
+  const renderedWrapper = document.querySelector('.js-code-sample-wrapper');
 
   codeSample.addEventListener('click', function() {
 
@@ -23,23 +14,18 @@ executeCodeSampleModal = () => {
 
     // Append the cloned code sample to the template body
     templateBody.appendChild(clone);
-let maybeabletouseavariablehere = clone;
+
     body.appendChild(templateWrapper);
   });
 };
 
 // Remove the modal using a click event
 closeModal = () => {
+  document.querySelector('.js-code-sample-modal-body').innerHTML = '';
   document.querySelector('.js-code-sample-wrapper').remove();
 }
 
-insertTemplate = () => {
-  return body.insertAdjacentHTML('beforeend', codeSampleString)
-}
-
-
 window.onload = () => {
-  insertTemplate();
   if (document.querySelector('.js-code-sample-template')) {
     return executeCodeSampleModal();
   } else {
