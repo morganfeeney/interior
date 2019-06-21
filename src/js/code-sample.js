a6b9d09da601895137665ed979eeda93fe4f11a3
Morgan Feeney <info@virtualsoup.co.uk>
2019-06-21 01:07:33 +0100

    Re-positioned code sample modal icon, ensured the modal can also be coxed via the overlay.

--- src/js/code-sample.js
+++ src/js/code-sample.js
@@ -1,19 +1,20 @@
-const codeSample = document.querySelector('.js-code-sample');
+const codeSampleModalButton = document.querySelector('.js-code-sample-open-modal');
+const template = document.querySelector('.js-code-sample-template');
 
 executeCodeSampleModal = () => {
-  const template = document.querySelector('.js-code-sample-template');
   const templateContent = document.importNode(template.content, true);
   const templateBody = templateContent.querySelector('.js-code-sample-modal-body');
   const templateWrapper = templateContent.querySelector('.js-code-sample-wrapper');
-  const renderedWrapper = document.querySelector('.js-code-sample-wrapper');
 
-  codeSample.addEventListener('click', function() {
+  codeSampleModalButton.addEventListener('click', function() {
 
+    const codeSample = this.closest('.js-placeholder-image').querySelector('.js-code-sample');
+    
     // Set the body to overflow hidden
-    body.classList.add('modal-open')
+    body.classList.add('modal-open');
 
     // Clone the code sample we want to display
-    const clone = this.cloneNode(true);
+    const clone = codeSample.cloneNode(true);
 
     // Get the distance from the top of the document and use it as an offset
     let offset = window.pageYOffset + this.getBoundingClientRect().top;
@@ -25,20 +26,37 @@ executeCodeSampleModal = () => {
     body.appendChild(templateWrapper);
 
     // document.querySelector('.js-code-sample-inner').style.setProperty('--code-sample-modal-offset', offset + "px");
+    closeModalViaOverlay();
   });
 };
 
 // Remove the modal using a click event
 closeModal = () => {
+  const modalBody = document.querySelector('.js-code-sample-modal-body');
+  const modalWrapper = document.querySelector('.js-code-sample-wrapper');
+
   body.classList.remove('modal-open');
-  document.querySelector('.js-code-sample-modal-body').innerHTML = '';
-  document.querySelector('.js-code-sample-wrapper').remove();
+
+  if (modalBody) {
+    modalBody.innerHTML = '';
+  }
+  if (modalWrapper) {
+    modalWrapper.remove();
+  };
+}
+
+closeModalViaOverlay = () => {
+  const renderedWrapper = document.querySelector('.js-code-sample-wrapper');
+  renderedWrapper.addEventListener('click', () => {
+    closeModal();
+  })
 }
 
 window.onload = () => {
-  if (document.querySelector('.js-code-sample-template')) {
-    return executeCodeSampleModal();
-  } else {
+  if (template) {
+    executeCodeSampleModal()
+  } 
+  else {
     return
   }
 }
