b9531cf4dd82a57201a0473dda4708bd56057f89
Morgan Feeney <info@virtualsoup.co.uk>
2019-09-06 00:34:35 +0100

    Changed hard-coded values to variables.

--- src/js/grid-row.js
+++ src/js/grid-row.js
@@ -1,19 +1,18 @@
 const rows = document.querySelectorAll('.type-body');
 const documentComputedFontSize = window.getComputedStyle(root).getPropertyValue('font-size');
 const getRowValue = window.getComputedStyle(root).getPropertyValue('--row-height');
-const rowComputedHeight = ((documentComputedFontSize.split('px')[0]) * (getRowValue.split('rem')[0]));
+const getGridRowGap = window.getComputedStyle(document.querySelector('.type-body').closest('.grid')).getPropertyValue('grid-row-gap');
+const gridRowGapUnit = getGridRowGap.split('px')[0];
+const rowHeightUnit = ((documentComputedFontSize.split('px')[0]) * (getRowValue.split('rem')[0]));
 
 gridRow = () => {
   rows.forEach(p => {
     let compStyles = window.getComputedStyle(p);
     let h = compStyles.getPropertyValue('height').split('px')[0];
 
-    if (h > rowComputedHeight) {
-      // The condition will always be true due to grid-auto-rows being used
-
-      let rowSpan = (Math.ceil((h - 32) / (rowComputedHeight + 16)));
-      // p.style.setProperty('--body-grid-row', `span ${rowSpan}`);
-
+    if (h > rowHeightUnit) {
+      // The condition will always be true due to grid-auto-rows being used.
+      let rowSpan = (Math.ceil((h - gridRowGapUnit) / (rowHeightUnit + (gridRowGapUnit / 2))));
       p.setAttribute('style', `grid-row: span ${rowSpan}`);
       
       // console.log(`Computed height of box: ${h}, a standard row height: ${rowComputedHeight}`)      
