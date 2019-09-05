66c73ea2db001d41cac86364de490e04cf2ee2d2
Morgan Feeney <morgan.feeney@jdwilliams.co.uk>
2019-09-05 10:33:06 +0100

    Changed .floor() for .ceil()

--- src/js/grid-row.js
+++ src/js/grid-row.js
@@ -1,19 +1,17 @@
-let rows = document.querySelectorAll('.type-body')
-let documentComputedFontSize = window.getComputedStyle(root).getPropertyValue('font-size');
-let getRowValue = window.getComputedStyle(root).getPropertyValue('--row-height');
-let rowComputedHeight = ((documentComputedFontSize.split('px')[0]) * (getRowValue.split('rem')[0]));
+const rows = document.querySelectorAll('.type-body');
+const documentComputedFontSize = window.getComputedStyle(root).getPropertyValue('font-size');
+const getRowValue = window.getComputedStyle(root).getPropertyValue('--row-height');
+const rowComputedHeight = ((documentComputedFontSize.split('px')[0]) * (getRowValue.split('rem')[0]));
 
 gridRow = () => {
   rows.forEach(p => {
     let compStyles = window.getComputedStyle(p);
     let h = compStyles.getPropertyValue('height').split('px')[0];
-    let lh = compStyles.getPropertyValue('line-height').split('px')[0];
-    let fs = compStyles.getPropertyValue('font-size').split('px')[0];
 
     if (h > rowComputedHeight) {
       // The condition will always be true due to grid-auto-rows being used
 
-      let rowSpan = (Math.floor((h - 32) / (rowComputedHeight + 16)) + 1);
+      let rowSpan = (Math.ceil((h - 32) / (rowComputedHeight + 16)));
       // p.style.setProperty('--body-grid-row', `span ${rowSpan}`);
 
       p.setAttribute('style', `grid-row: span ${rowSpan}`);
@@ -23,4 +21,4 @@ gridRow = () => {
   });
 }
 
-gridRow()
+gridRow();
