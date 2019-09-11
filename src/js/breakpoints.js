eceeddb8c7750c2e4daf45709a3aa7b3e4d0b1a8
Morgan Feeney <morgan.feeney@jdwilliams.co.uk>
2019-09-11 16:19:55 +0100

    Working on matchmedia and matches

--- src/js/breakpoints.js
+++ src/js/breakpoints.js
@@ -6,3 +6,12 @@ export const customMediaMin = {
   '--desktop': '(min-width: 1440px)',
   '--appleTV': '(min-width: 1920px)',
 };
+
+export const customMediaMinMax = {
+  '--iPhoneSE': '(min-width: 320px) and (max-width: 413px)',
+  '--iPhoneXR': '(min-width: 414px) and (max-width: 767px)',
+  '--tablet': '(min-width: 768px) and (max-width: 1024px)',
+  '--iPadPro': '(min-width: 1024px) and (max-width: 1439px)',
+  '--desktop': '(min-width: 1440px) and (max-width: 1919px)',
+  '--appleTV': '(min-width: 1920px)',
+};
--- src/js/grid-row.js
+++ src/js/grid-row.js
@@ -1,4 +1,4 @@
-import { customMediaMin } from './breakpoints.js';
+import { customMediaMin, customMediaMinMax } from './breakpoints.js';
 
 const rows = document.querySelectorAll('.type-body');
 const documentComputedFontSize = window.getComputedStyle(root).getPropertyValue('font-size');
@@ -8,53 +8,47 @@ const gridRowGapUnit = getGridRowGap.split('px')[0];
 const rowHeightUnit = ((documentComputedFontSize.split('px')[0]) * (getRowValue.split('rem')[0]));
 
 function gridRow(p) {
+  p.style.removeProperty('--body-grid-row');
   let compStyles = window.getComputedStyle(p);
   let h = compStyles.getPropertyValue('height').split('px')[0];
-
+  console.log(h);
   if (h > rowHeightUnit) {
     // The condition will always be true due to grid-auto-rows being used.
     let rowSpan = (Math.ceil(h / (rowHeightUnit + (gridRowGapUnit / 2))));
-    // console.log(rowSpan);
     p.style.setProperty('--body-grid-row', `span ${rowSpan}`);
-    // console.log(`Computed height of box: ${h}, a standard row height: ${rowComputedHeight}`)      
+    // console.log(`Computed height of box: ${h}, a standard row height: ${rowHeightUnit}`)
   }
 }
 
-// NEED TO MAKE THIS WORK FOR A SINGLE INSTANCE TO TEST IT OUT!!! READ UP ON MATCHMEDIA ONCE AGAIN!!!
-
-function setUpRows(elements) {
-  elements.forEach(e => {
-    gridRow(e);
-    // console.log(e);
-    for (const media in customMediaMin) {
-      const m = window.matchMedia(customMediaMin[media]);
-      if (m.matches) {
-        window.addEventListener(
-          "change", 
-          gridRow(e)
-        )
-      }
-    }
-  })
+function mediaMatched() {
+  rows.forEach(row => {
+    gridRow(row)
+  });
 }
 
-setUpRows(rows)
-
-function gridRowMediaListener(event) {
-  if (event.matches) {
-    window.addEventListener(
-      "change", 
-      gridRow(event)
-    )
+function matchesIt(blah) {
+  if (blah.matches) {
+    window.addEventListener('change', mediaMatched())
   }
 }
 
-function mediaLoop() {
-  for (const media in customMediaMin) {
-    const m = window.matchMedia(customMediaMin[media]);
-    m.addListener(gridRowMediaListener); 
+function initCustomMedia(){
+  for (const media in customMediaMinMax) {
+    let myMedia = window.matchMedia(customMediaMinMax[media])
+
+    myMedia.addListener(matchesIt)
   }
 }
 
+initCustomMedia()
 
-// window.matchMedia(customMediaMin['--iPhoneSE']).addListener(gridRowMediaListener)
+// gridRow(document.querySelector('.type-body'));
+
+// setUpRows(rows)
+
+// function mediaLoop() {
+//   for (const media in customMediaMin) {
+//     const m = window.matchMedia(customMediaMin[media]);
+//     m.addListener(gridRowMediaListener); 
+//   }
+// }
