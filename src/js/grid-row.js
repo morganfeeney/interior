25fb6343a0887953b3963430ff490a6795a01285
Morgan Feeney <info@virtualsoup.co.uk>
2019-09-12 11:37:46 +0100

    The variables needed to be re-declared.

--- src/js/grid-row.js
+++ src/js/grid-row.js
@@ -1,20 +1,34 @@
-import { customMediaMin, customMediaMinMax } from './breakpoints.js';
+import { 
+  customMediaMin, 
+  customMediaMinMax 
+} 
+from './breakpoints.js';
 
 const rows = document.querySelectorAll('.type-body');
-const documentComputedFontSize = window.getComputedStyle(root).getPropertyValue('font-size');
-const getRowValue = window.getComputedStyle(root).getPropertyValue('--row-height');
-const getGridRowGap = window.getComputedStyle(document.querySelector('.type-body').closest('.grid')).getPropertyValue('grid-row-gap');
-const gridRowGapUnit = getGridRowGap.split('px')[0];
-const rowHeightUnit = ((documentComputedFontSize.split('px')[0]) * (getRowValue.split('rem')[0]));
+let documentComputedFontSize = '';
+let getRowValue = '';
+let getGridRowGap = '';
+let gridRowGapUnit = '';
+let rowHeightUnit = '';
+
+function setUpRowVars() {
+  documentComputedFontSize = window.getComputedStyle(root).getPropertyValue('font-size');
+  getRowValue = window.getComputedStyle(root).getPropertyValue('--rh');
+  getGridRowGap = window.getComputedStyle(document.querySelector('.type-body').closest('.grid')).getPropertyValue('grid-row-gap');
+  gridRowGapUnit = getGridRowGap.split('px')[0];
+  rowHeightUnit = ((documentComputedFontSize.split('px')[0]) * (getRowValue.split('rem')[0]));
+}
 
 function gridRow(p) {
+  setUpRowVars();
+  
   p.style.removeProperty('--body-grid-row');
   let compStyles = window.getComputedStyle(p);
   let h = compStyles.getPropertyValue('height').split('px')[0];
-  console.log(h);
+  let rowSpan = (Math.ceil(h / (rowHeightUnit + (gridRowGapUnit / 2))));
+
   if (h > rowHeightUnit) {
     // The condition will always be true due to grid-auto-rows being used.
-    let rowSpan = (Math.ceil(h / (rowHeightUnit + (gridRowGapUnit / 2))));
     p.style.setProperty('--body-grid-row', `span ${rowSpan}`);
     // console.log(`Computed height of box: ${h}, a standard row height: ${rowHeightUnit}`)
   }
@@ -36,6 +50,10 @@ function initCustomMedia(){
   for (const media in customMediaMinMax) {
     let myMedia = window.matchMedia(customMediaMinMax[media])
 
+    if (myMedia.matches) {
+      mediaMatched()
+    }
+    
     myMedia.addListener(matchesIt)
   }
 }
