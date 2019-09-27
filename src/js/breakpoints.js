837f71a152e392a5fca2036c806d2b6ac85ffaa1
Morgan Feeney <info@virtualsoup.co.uk>
2019-09-27 16:47:07 +0100

    Whitespace.

--- src/js/breakpoints.js
+++ src/js/breakpoints.js
@@ -1,19 +1,19 @@
 const customMediaMin = {
-  '--iPhoneSE':     '(min-width: 320px)',
-  '--iPhoneXR':     '(min-width: 414px)',
-  '--tablet':       '(min-width: 768px)',
-  '--iPadPro':      '(min-width: 1024px)',
-  '--desktop':      '(min-width: 1440px)',
-  '--appleTV':      '(min-width: 1920px)',
+  '--iPhoneSE': '(min-width: 320px)',
+  '--iPhoneXR': '(min-width: 414px)',
+  '--tablet': '(min-width: 768px)',
+  '--iPadPro': '(min-width: 1024px)',
+  '--desktop': '(min-width: 1440px)',
+  '--appleTV': '(min-width: 1920px)',
 };
 
 const customMediaMax = {
   '--iPhoneSE-max': '(max-width: 319px)',
   '--iPhoneXR-max': '(max-width: 413px)',
-  '--tablet-max':   '(max-width: 767px)',
-  '--iPadPro-max':  '(max-width: 1023px)',
-  '--desktop-max':  '(max-width: 1439px)',
-  '--appleTV-max':  '(max-width: 1919px)',
+  '--tablet-max': '(max-width: 767px)',
+  '--iPadPro-max': '(max-width: 1023px)',
+  '--desktop-max': '(max-width: 1439px)',
+  '--appleTV-max': '(max-width: 1919px)',
 };
 
 const customMediaMinMax = {
