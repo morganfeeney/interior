6ad58c8e8eb66d9b21b7d1081e14a00bea83f6bc
Morgan Feeney <info@virtualsoup.co.uk>
2019-09-27 17:11:04 +0100

    Created a utility function for checking what media width is currently in use.

---
+++ src/js/display-media-info.js
@@ -0,0 +1,23 @@
+import { customMediaMinMax } from './breakpoints.js';
+
+export function displyMediaInfo() {
+  for (const media in customMediaMinMax) {
+    // Destructure variables from customMediaMinMax object.
+    let { breakpoint } = customMediaMinMax[media];
+    const m = window.matchMedia(breakpoint);
+    
+    // Display media info on page load.
+    if (m.matches) {
+      console.log(`${media}: ${breakpoint}`);
+    }
+
+    // Display media info on change.
+    m.addEventListener('change', (e) => {
+      if (e.matches) {
+        console.log(`${media}: ${breakpoint}`);
+      }
+    });
+  }
+}
+
+displyMediaInfo()
